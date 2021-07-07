5523d5177597a44c09b57b41bfaedd2e1a46abb3
1the-Ji <jish0516@gmail.com>
2021-07-07 23:02:44 +0900

    jpa main -> 기본 문법 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,9 +11,25 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
 
-        em.clear();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try{
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
+            Member findMember = em.find(Member.class,1L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//            em.remove(findMember);
+            findMember.setName("HelloJPA_Update");
 
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.clear();
+        }
         emf.close();
-
     }
 }
